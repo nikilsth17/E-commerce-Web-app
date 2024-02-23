17136b18962b1a2f4addc800101e368306ab6475
nikilsth17 <nikillawo7@gmail.com>
2024-02-23 20:36:12 +0545

    feat:responsive

--- ecommerce-ui/src/pages/Product.jsx
+++ ecommerce-ui/src/pages/Product.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearProductFilter, openProductFilter, setSearchText } from '../store/slice/productSlice';
-
+import background from "../assets/image.avif"
 
 const Product = () => {
     const userRole =localStorage.getItem("userRole");
@@ -24,12 +24,15 @@ const Product = () => {
         xs: "3rem",
         sm: "3rem",
       },
+      backgroundImage:`url(${background})`,
+      backgroundRepeat:"no-repeat",
+      backgroundSize:"cover"
       }}
     >
     
       <Box sx={{
           display: "flex",
-          flexDirection: "row",
+          flexDirection: {sm:"row",xs:"column"},
           justifyContent: {
             xs: "center",
             sm: "center",
@@ -45,71 +48,70 @@ const Product = () => {
       }}>
 
 
-{userRole==="buyer" && (
-  <>
-    <Button 
-      variant='outlined' 
-      disabled={!useHasAppliedFilter} 
-      onClick={()=>dispatch(clearProductFilter())}
-      sx={{
-        mb:"2rem", 
-        display:"flex", 
-        alignItems:"center",
-        marginRight:{xs:0,sm:"2%"} 
-      }}
-    >
-      Clear 
-    </Button>
-    <Button variant='outlined' 
-      sx={{
-        mb:"2rem", 
-        display:"flex", 
-        alignItems:"center",
-        marginRight:{xs:0,sm:"35%"} 
-      }}
-      onClick={()=>dispatch(openProductFilter())}>
-        Filter
-    </Button>
-  </>
-      
-)}
+          {userRole==="buyer" && (
+            <>
+              <Button 
+                variant='outlined' 
+                disabled={!useHasAppliedFilter} 
+                onClick={()=>dispatch(clearProductFilter())}
+                sx={{
+                  mb:"2rem", 
+                  display:"flex", 
+                  alignItems:"center",
+                  marginRight:{xs:0,sm:"2%"} 
+                }}
+              >
+                Clear 
+              </Button>
+              <Button variant='outlined' 
+                sx={{
+                  mb:"2rem", 
+                  display:"flex", 
+                  alignItems:"center",
+                  marginRight:{xs:0,sm:"35%"} 
+                }}
+                onClick={()=>dispatch(openProductFilter())}>
+                  Filter
+              </Button>
+            </>
+                
+          )}
 
 
-    <Paper 
-      sx={{ 
-        mb:"2rem", 
-        display: 'flex', 
-        justifyContent:"center",
-        alignItems: 'center', 
-        width: 250,
-        marginRight:{xs:0,sm:"25%"} 
-      }}
-      onChange={(e)=>dispatch(setSearchText(e.target.value))}
+          <Paper 
+            sx={{ 
+              mb:"2rem", 
+              display: 'flex', 
+              justifyContent:"space-between",
+              alignItems: 'center', 
+              width: 250,
+              marginRight:{xs:0,sm:"25%"} 
 
-    >
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search Products"
-        inputProps={"aria-label"}
-      />
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton type="button" aria-label="search">
-        <SearchIcon />
-      </IconButton>
-    </Paper>
+            }}
+            onChange={(e)=>dispatch(setSearchText(e.target.value))}
+
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search Products"
+              inputProps={"aria-label"}
+            />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton type="button" aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
     
-        {
-          userRole==="seller" &&   
-          <Button variant='contained' 
-          onClick={()=>naviagate("/product/add")} 
-          sx={{
-            gap:"2rem",
-            marginBottom:"2rem"
-          }}
-        ><HiOutlineViewGridAdd size={"1.5rem"}/> Product</Button>
-        }
-      
-   
+          {
+            userRole==="seller" &&   
+            <Button variant='contained' 
+            onClick={()=>naviagate("/product/add")} 
+            sx={{
+              gap:"2rem",
+              marginBottom:"2rem"
+            }}
+          ><HiOutlineViewGridAdd size={"1.5rem"}/> Product</Button>
+          }
       </Box>
    
       {userRole==="seller"? <SellerProductList/>:<BuyerProductList/>}
