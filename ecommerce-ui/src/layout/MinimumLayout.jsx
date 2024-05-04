aec6749bc08b3d0c567cfb97bc7fa763bca66c1c
nikilsth17 <nikillawo7@gmail.com>
2024-05-04 20:09:48 +0545

    change

--- ecommerce-ui/src/layout/MinimumLayout.jsx
+++ ecommerce-ui/src/layout/MinimumLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+// import { Outlet } from 'react-router-dom'
 import CustomSnackbar from '../component/CustomSnackbar'
+import Login from '../pages/Login'
 
 const MinimumLayout = () => {
   return (
     <>
     <CustomSnackbar/>
-        <Outlet/>
+        <Login/>
     </>
   )
 }
